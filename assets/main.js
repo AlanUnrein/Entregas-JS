c86d396b75934a380c8abe25c835a21b9e41a7de
unknown <alandanielunrein@gmail.com>
2022-09-19 17:46:39 -0300

    tercer entrega

--- assets/main.js
+++ assets/main.js
@@ -1,102 +1,114 @@
 const Pizzas = [
-    {
-        nombre : 'muzzarella',
-        id : 1,
-        precio : 100,
-        ingredientes : ['muzzarella', 'salsa']
-    },
-    {
-        nombre : 'napolitana',
-        id : 2,
-        precio : 110,
-        ingredientes : ['tomate', 'jamon', 'salsa']
-    },
-    {
-        nombre : 'salchicha',
-        id : 3,
-        precio : 110,
-        ingredientes : ['salchicha', 'salsa', 'queso']
-    },
-    {
-        nombre : 'calabresa',
-        id : 4,
-        precio : 130,
-        ingredientes : ['calabresa', 'queso', 'salsa']
-    },
-    {
-        nombre : 'fugazzeta',
-        id : 5,
-        precio : 100,
-        ingredientes : ['cebolla', 'queso', 'salsa']
-    },
-    {
-        nombre : 'jamon y morron',
-        id : 6,
-        precio : 100,
-        ingredientes : ['jamon', 'queso', 'morron', 'salsa']
-    },
+  {
+      nombre : 'muzzarella',
+      id : 1,
+      precio : 100,
+      ingredientes : ['muzzarella', 'salsa'],
+      img : 'muzzarella.jpg'
+  },
+  {
+      nombre : 'napolitana',
+      id : 2,
+      precio : 110,
+      ingredientes : ['tomate', 'jamon', 'salsa'],
+      img : 'napolitana.jpg'
+  },
+  {
+      nombre : 'salchicha',
+      id : 3,
+      precio : 110,
+      ingredientes : ['salchicha', 'salsa', 'queso'],
+      img : 'salchicha.png'
+  },
+  {
+      nombre : 'calabresa',
+      id : 4,
+      precio : 130,
+      ingredientes : ['calabresa', 'queso', 'salsa'],
+      img : 'calabresa.jpg'
+  },
+  {
+      nombre : 'fugazzeta',
+      id : 5,
+      precio : 100,
+      ingredientes : ['cebolla', 'queso', 'salsa'],
+      img : 'fugazzetta.jpg'
+  },
+  {
+      nombre : 'jamon y morron',
+      id : 6,
+      precio : 100,
+      ingredientes : ['jamon', 'queso', 'morron', 'salsa'],
+      img : 'jamon y morron.jpg'
+  },
 ]
 
 
-const inputNumber = document.getElementById('inputNumber')
-const form = document.getElementById('form')
-const h2Nombre = document.querySelector('h2')
-const h4Precio = document.querySelector('h4')
-const small = document.querySelector("small")
-
+const inputNumber = document.getElementById('inputNumber');
+const form = document.getElementById('form');
+const card = document.querySelector('.card');
+const ul = document.querySelector('.ul')
 
 
 let pizzaFiltrada = [];
 
+const saveLocalStorage = pizzaList => {
+localStorage.setItem('Pizzas', JSON.stringify(pizzaList))
+}
+
+
+
 const showPizza = () => {
-    let pizza = pizzaFiltrada
+let pizza = pizzaFiltrada;
+let {nombre, id, precio, ingredientes, img} = pizza;
+ingredientes = ingredientes.join(' - ')
 
-    small.textContent = ''
-    h2Nombre.textContent = `Proba nuestra pizza de ${pizza.nombre}`
-    h2Nombre.classList.add('h2')
-    h2Nombre.classList.remove('hidden')
-
-    h4Precio.textContent = `A tan solo $${pizza.precio}`
-    h4Precio.classList.add('h4')
-    h4Precio.classList.remove('hidden')
-
-    inputNumber.value = 0
-    
+card.innerHTML = `
+<div class="container--card">
+  <small class="hidden"></small>
+  <h2 class="h2">Proba nuestra pizza de ${nombre}.</h2>
+  <img class="container--img"src="./assets/img/${img}" alt="pizza de ${pizza.nombre}">
+  <\div>
+  <p class="p">${ingredientes}</p>
+  <h4 class="h4">A tan solo $${precio}.</h4>
+  `
 }
 
 const filtrarID = () => {
-  const filterId = inputNumber.value;
+const filterId = inputNumber.value;
+let pizzaID = Pizzas.find(pizza => pizza.id == filterId);
 
-  if(!filterId.length){   
-      small.textContent = 'Error, no ingresaste un ID';
-      h2Nombre.classList.add('hidden');
-      h4Precio.classList.add('hidden') ;
-      pizzaFiltrada = [];
-      inputNumber.value = 0;
-      return;
+if(!filterId.length || filterId == 0){   
+  card.style.border = 'none'
+  card.innerHTML = `
+      <small>No ingresaste ninguna ID.</small>
+      <h2 class="hidden"></h2>
+      <h4 class="hidden"></h4>
+      <img class="hidden" src="">
+      `
+    return;
 
-    } else if(filterId < 1 || filterId > 6){
-      small.textContent = 'No se encuentran pizzas con ese ID'; 
-      h2Nombre.classList.add('hidden'); 
-      h4Precio.classList.add('hidden');
-      pizzaFiltrada = [];
-      inputNumber.value = 0;
-      return;
+  } else if(!pizzaID){
+    card.innerHTML = `
+      <small>Lo sentimos!, no disponemos de pizzas con ese ID.</small>
+      <h2 class="hidden"></h2>
+      <h4 class="hidden"></h4>
+      <img class="hidden" src="">
+      `
+    return;
 
-    } else if( pizzaFiltrada = Pizzas.find(pizza => pizza.id === parseInt(filterId))
-    ) showPizza();
-  }
-    
+  } else (pizzaFiltrada = pizzaID);
+    showPizza();
+}
+  
 
 const init = () => {
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    filtrarID();
-  })
-  }
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  filtrarID();
+  saveLocalStorage(Pizzas)
+})
+}
 
 
 init();
-
-
-
